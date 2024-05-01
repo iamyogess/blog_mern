d9dd7498abbcaf7091043af480e5d220c61327c2
Yogesh <iamyogess@gmail.com>
2024-05-01 23:09:18 +0545

    responsive header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
-    <>
+    <div className="App font-opensans">
       <HomePage />
-    </>
+    </div>
   );
 };
 
