13b03e9287f8df3990f9c2037cf163f08c8676f5
Yogesh <iamyogess@gmail.com>
2024-05-17 21:13:38 +0545

    setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+//middlewares
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}/`);
+});
