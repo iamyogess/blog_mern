ee51db117df1afca00c2e6ea95030e9f6b2bf22c
Yogesh Shrestha <iamyogess@gmail.com>
2024-05-19 21:59:12 +0545

    user registration

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,26 @@ dotenv.config();
 
 import connectDB from "./config/db.js";
 
+//routes
+import userRoutes from "./routes/userRoutes.js";
+
 const app = express();
-
-app.use(express.json());
-
 const PORT = process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json());
+app.use("/api/users", userRoutes);
 
 connectDB();
 
-app.get("/", (req,res)=>{
-    res.send("hi");
-})
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.get("/hi", (req, res) => {
+  res.send("hello k 6?");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:5000`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
