7a3ab45e77a4daf06b928125afdf9f2494ed19cc
Yogesh Shrestha <iamyogess@gmail.com>
2024-05-19 21:18:42 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
 
+import connectDB from "./config/db.js";
+
 const app = express();
-//middlewares
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+connectDB();
+
+app.get("/", (req,res)=>{
+    res.send("hi");
+})
 
 app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}/`);
+  console.log(`Server started at http://localhost:5000`);
 });
