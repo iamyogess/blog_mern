6e01524e027a3ac16b9e64b983c8aaf185b99840
Yogesh Shrestha <iamyogess@gmail.com>
2024-05-21 21:56:54 +0545

    protected route in profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -57,3 +57,25 @@ export const loginUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const userProfile = async (req, res, next) => {
+  try {
+    let user = await User.findById(req.user._id);
+    if (user) {
+      return res.status(201).json({
+        _id: user.id,
+        avatar: user.avatar,
+        name: user.name,
+        email: user.email,
+        verified: user.verified,
+        admin: user.admin,
+      });
+    } else {
+      let error = new Error("User not found!");
+      error.statusCode = 404;
+      next(error);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
+import {
+  registerUser,
+  loginUser,
+  userProfile,
+} from "../controllers/userController.js";
+import { authGuard } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", authGuard, userProfile);
 
 export default router;
