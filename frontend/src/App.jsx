785e8157c15d4f6d3ed16a360b941de825b22606
Yogesh Shrestha <iamyogess@gmail.com>
2024-06-15 21:39:56 +0545

    single page navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <div className="App font-opensans">
       <Routes>     
         <Route index path="/" element={<HomePage />} />
-        <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/blog/:slug" element={<ArticleDetailPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -3,27 +3,32 @@ import { images, stables } from "./../constants";
 
 import { BsCheckLg } from "react-icons/bs";
 import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const ArticleCard = ({ className, post }) => {
   return (
     <div
       className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}
     >
-      <img
-        src={
-          post.photo
-            ? stables.UPLOAD_FOLDER_BASE_URL + post.photo
-            : images.samplePostImage
-        }
-        alt="title"
-        className="w-full object-cover object-center h-auto mg:h-52 lg:h-48 xl:h-60"
-      />
+      <Link to={`/blog/${post.slug}`}>
+        <img
+          src={
+            post.photo
+              ? stables.UPLOAD_FOLDER_BASE_URL + post.photo
+              : images.samplePostImage
+          }
+          alt="title"
+          className="w-full object-cover object-center h-auto mg:h-52 lg:h-48 xl:h-60"
+        />
+      </Link>
 
       <div className="p-5">
-        <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
-          {post.title}
-        </h2>
-        <p className="text-dark-light text-sm md:text-lg">{post.caption}</p>
+        <Link to={`/blog/${post.slug}`}>
+          <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
+            {post.title}
+          </h2>
+          <p className="text-dark-light text-sm md:text-lg">{post.caption}</p>
+        </Link>
 
         <div className="flex justify-between flex-nowrap items-center mt-6">
           <div className="flex items-center gap-x-2 gap-x-2.5">
@@ -42,12 +47,15 @@ const ArticleCard = ({ className, post }) => {
               </h4>
               <div className="flex items-center gap-x-2">
                 <span
-                 className={`${post.user.verified ? "bg-[#36B37E]" : "bg-red-500"} w-fit bg-opacity-20 p-1.5 rounded-full`}
-
+                  className={`${
+                    post.user.verified ? "bg-[#36B37E]" : "bg-red-500"
+                  } w-fit bg-opacity-20 p-1.5 rounded-full`}
                 >
-                  {post.user.verified ? <BsCheckLg className="h-1.5 w-1.5 text-[#36B37E]" /> : <AiOutlineClose className="h-1.5 w-1.5 "/>}
-                  
-            
+                  {post.user.verified ? (
+                    <BsCheckLg className="h-1.5 w-1.5 text-[#36B37E]" />
+                  ) : (
+                    <AiOutlineClose className="h-1.5 w-1.5 " />
+                  )}
                 </span>
                 <span className="italic text-dark-light text-xs md:text-sm">
                   {post.user.verified ? "Verified" : "Unverified"} Writer
@@ -56,7 +64,10 @@ const ArticleCard = ({ className, post }) => {
             </dir>
           </div>
           <span className="font-bold text-dark-light italic text-sm md:text-base">
-           {new Date(post.createdAt).getDate()} {new Date(post.createdAt).toLocaleString("default",{month:"long"})}
+            {new Date(post.createdAt).getDate()}{" "}
+            {new Date(post.createdAt).toLocaleString("default", {
+              month: "long",
+            })}
           </span>
         </div>
       </div>
